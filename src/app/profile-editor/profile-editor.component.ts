0683159613129bfe1ce81372a752a29e4c616c59
emekbib <emekbib@students.kennesaw.edu>
2021-10-03 19:53:51 -0400

    Capstone Zipcode Edit autipopulate

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,40 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
-
-
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
-export class ProfileEditorComponent {
-  profileForm = new FormGroup({
-
-    contactName: new FormControl('', Validators.required),
-
-    contactJob: new FormControl('',Validators.required),
-
-    contactEmail: new FormControl('',Validators.required),
-
-    contactPhone: new FormControl('',Validators.required),
-
-    comapnyName: new FormControl('',Validators.required),
-
-    organAddress: new FormControl('',Validators.required),
-
-    organWebsite: new FormControl('',Validators.required),
-
+export class ProfileEditorComponent implements OnInit {
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: this.fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required]
+    }),
   });
 
+  foundZipCity() {
+    let postal_code = this.profileForm.get("address")?.get("zip")?.value;
+
+    let foundZipCity = data.find(data => data.zip_code == postal_code);
+    this.profileForm.patchValue({
+      address: {
+        city: foundZipCity?.city
+      }
+    });
+  }
+
+  constructor(private fb: FormBuilder) { }
+  
+  onZipChange(){
+    
+  }
+  ngOnInit(): void {
+  }
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ 
 }
+
+let data = [
+
+  {
+    zip_code: 30144,
+    city: "Kennesaw",
+    state: "GA"
+  },
+
+  {
+    zip_code: 30152,
+    city: "Kennesaw",
+    state: "GA"
+  },
+
+  {
+    zip_code: 30060,
+    city: "Marietta",
+    state: "GA"
+  },
+
+  {
+    zip_code: 30061,
+    city: "Marietta",
+    state: "GA"
+  },
+
+];
